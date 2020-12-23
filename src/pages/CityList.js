2e3a504f2778d739a5cde80c42ae11691a52bd4a
wb_yangxp <wb_yangxp@yiche.com>
2020-12-23 14:10:57 +0800

    引入百度地图

--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -1,11 +1,22 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import "./CityList.css";
 
 class CityList extends React.PureComponent {
+    renderMap = () => {
+        const { BMapGL } = window;
+        var map = new BMapGL.Map("container");
+        var point = new BMapGL.Point(116.404, 39.915);
+        map.centerAndZoom(point, 15);
+    }
+    componentDidMount() {
+        this.renderMap();
+    }
     render() {
-        console.log(this.props)
         return (
-            <div>CityList</div>
+            <div className="cityList">
+                <div id="container"></div>
+            </div>
         )
     }
 }
